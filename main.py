69b0598589ff59da77455afad6bb4e27118b5b33
Nicole-Palomino <alessa.kz.fx@gmail.com>
2024-05-21 16:23:18 -0500

    Leccion 2: render template

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def index():
+    user_ip_information = request.remote_addr
+    response = make_response( redirect('/show_information_address') )
+    response.set_cookie('user_ip_information', user_ip_information)
 
-app.run(host='0.0.0.0', port=81)
+    return response
+
+@app.route('/show_information_address')
+def show_information():
+    user_ip = request.cookies.get('user_ip_information')
+    return render_template('ip_information.html', user_ip = user_ip)
+
+app.run(host='0.0.0.0', port=81, debug=True)
