9faa219ad4e35828a1041a8d8d88d74fcba55f6b
Nicole-Palomino <alessa.kz.fx@gmail.com>
2024-05-23 22:33:18 -0500

    Lección 3: Templates y macros en jinja

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+items = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
+
+@app.route('/index')
 def index():
     user_ip_information = request.remote_addr
     response = make_response( redirect('/show_information_address') )
@@ -13,6 +15,12 @@ def index():
 @app.route('/show_information_address')
 def show_information():
     user_ip = request.cookies.get('user_ip_information')
-    return render_template('ip_information.html', user_ip = user_ip)
+    
+    context = {
+        "user_ip": user_ip,
+        "items": items
+    }
+
+    return render_template('ip_information.html', **context)
 
 app.run(host='0.0.0.0', port=81, debug=True)
