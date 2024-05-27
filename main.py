0e688e3b0f174901334dc9b1cf444652d0109c4e
Nicole-Palomino <alessa.kz.fx@gmail.com>
2024-05-26 22:11:29 -0500

    Lección 5: template error

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 items = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
 
+@app.errorhandler(404)
+def not_found_endpoint(error):
+    return render_template('404.html', error= error)
+
 @app.route('/index')
 def index():
     user_ip_information = request.remote_addr
